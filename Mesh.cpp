dbcf19838cd8bd9cad7b30da446bbb2d491d6cb1
Nathan <nvm7252@rit.edu>
2025-01-28 10:49:29 -0500

    Moved draw code to mesh class

--- Mesh.cpp
+++ Mesh.cpp
@@ -64,6 +64,34 @@ Mesh::~Mesh() {
 	//If cleanup required, place code here
 }
 
+//Draw the Geometry
+void Mesh::Draw() {
+	// DRAW geometry
+	// - These steps are generally repeated for EACH object you draw
+	// - Other Direct3D calls will also be necessary to do more complex things
+
+	// Set buffers in the input assembler (IA) stage
+	//  - Do this ONCE PER OBJECT, since each object may have different geometry
+	//  - For this demo, this step *could* simply be done once during Init()
+	//  - However, this needs to be done between EACH DrawIndexed() call
+	//     when drawing different geometry, so it's here as an example
+	UINT stride = sizeof(Vertex);
+	UINT offset = 0;
+	Graphics::Context->IASetVertexBuffers(0, 1, vertexBuffer.GetAddressOf(), &stride, &offset);
+	Graphics::Context->IASetIndexBuffer(indexBuffer.Get(), DXGI_FORMAT_R32_UINT, 0);
+
+	// Tell Direct3D to draw
+	//  - Begins the rendering pipeline on the GPU
+	//  - Do this ONCE PER OBJECT you intend to draw
+	//  - This will use all currently set Direct3D resources (shaders, buffers, etc)
+	//  - DrawIndexed() uses the currently set INDEX BUFFER to look up corresponding
+	//     vertices in the currently set VERTEX BUFFER
+	Graphics::Context->DrawIndexed(
+		indices,    // The number of indices to use (we could draw a subset if we wanted)
+		0,			// Offset to the first index we want to use
+		0);			// Offset to add to each index when looking up vertices
+}
+
 //Return whole ComPtr Objects
 Microsoft::WRL::ComPtr<ID3D11Buffer> Mesh::GetVertexBuffer() const {
 	return vertexBuffer;

861b3ab7979ae396569767704072492639aab161
Nathan <nvm7252@rit.edu>
2025-02-08 20:22:11 -0500

    completed setters

--- Transform.cpp
+++ Transform.cpp
@@ -2,7 +2,7 @@
 using namespace DirectX;
 
 Transform::Transform() :
-	position(0, 0, 0), scale(1, 1, 1), pitchYawRoll(0, 0, 0)
+	position(0, 0, 0), scale(1, 1, 1), pitchYawRoll(0, 0, 0), dirty(false)
 {
 	//store matrices
 	XMStoreFloat4x4(&world, XMMatrixIdentity());
@@ -16,26 +16,37 @@ Transform::~Transform()
 
 void Transform::SetPosition(float x, float y, float z)
 {
+	SetPosition(XMFLOAT3(x, y, z));
 }
 
 void Transform::SetPosition(DirectX::XMFLOAT3 newPos)
 {
+	XMStoreFloat3(&position, XMLoadFloat3(&newPos));
+	dirty = true;
 }
 
 void Transform::SetRotation(float pitch, float yaw, float roll)
 {
+	//call method overload with xmfloat compatibility
+	SetRotation(XMFLOAT3(pitch, yaw, roll));
 }
 
 void Transform::SetRotation(DirectX::XMFLOAT3 newRot)
 {
+	//create xmvector and store result in original position
+	XMStoreFloat3(&pitchYawRoll, XMLoadFloat3(&newRot));
+	dirty = true;
 }
 
 void Transform::SetScale(float x, float y, float z)
 {
+	SetScale(XMFLOAT3(x, y, z));
 }
 
 void Transform::SetScale(DirectX::XMFLOAT3 newScale)
 {
+	XMStoreFloat3(&scale, XMLoadFloat3(&newScale));
+	dirty = true;
 }
 
 DirectX::XMFLOAT3 Transform::GetPosition()

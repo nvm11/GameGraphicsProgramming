c985d3203f8ce668ebd5119523b8f3aec0418cfb
Nathan <nvm7252@rit.edu>
2025-02-15 13:24:44 -0500

    added fields in camera

--- Camera.h
+++ Camera.h
@@ -4,5 +4,17 @@
 
 class Camera
 {
+private:
+	//Fields
+	//Transformations
+	Transform transform;
+	//Viewing
+	DirectX::XMFLOAT4X4 view;
+	DirectX::XMFLOAT4X4 projection;
+	//Customization of viewing
+	float fovAngle; //in radians
+	float nearClipDist;
+	float farClipDist;
+	bool isPerspective;
 };
 

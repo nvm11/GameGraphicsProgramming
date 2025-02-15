fa9222d7a0ad791bd2e8817830a08e9720b6e7e9
Nathan <nvm7252@rit.edu>
2025-02-15 16:35:06 -0500

    Implemented constructor and perspective portion of proj matrix method

--- Camera.cpp
+++ Camera.cpp
@@ -1,1 +1,52 @@
 #include "Camera.h"
+using namespace DirectX;
+
+Camera::Camera(XMFLOAT3 initialPos, float fov, float aspectRatio, float nearClipDist, float farClipDist, bool perspective)
+	:fovAngle(fov), nearClipDist(nearClipDist), farClipDist(farClipDist), isPerspective(perspective)
+{
+	transform.SetPosition(initialPos);
+	UpdateViewMatrix();
+	UpdateProjectionMatrix(aspectRatio);
+}
+
+Camera::~Camera()
+{
+	//Memory cleanup if needed
+}
+
+XMFLOAT4X4 Camera::GetView()
+{
+	return view;
+}
+
+XMFLOAT4X4 Camera::GetProjection()
+{
+	return projection;
+}
+
+void Camera::UpdateProjectionMatrix(float aspect)
+{
+	//create temp math-type matix
+	XMMATRIX projMatrix;
+
+	//check projection type
+	if (isPerspective) {
+		projMatrix = XMMatrixPerspectiveFovLH(fovAngle,
+			aspect,
+			nearClipDist,
+			farClipDist);
+	}
+	else {
+
+	}
+
+	//store result
+}
+
+void Camera::UpdateViewMatrix()
+{
+}
+
+void Camera::Update(float dt)
+{
+}
--- Camera.h
+++ Camera.h
@@ -16,5 +16,31 @@ private:
 	float nearClipDist;
 	float farClipDist;
 	bool isPerspective;
+
+public:
+	//Constructor
+	Camera(DirectX::XMFLOAT3 initialPos,
+		float fov = DirectX::XM_PIDIV2,
+		float apsectRatio,
+		float nearClipDist = 0.01f,
+		float farClipDist = 100.0f,
+		bool perspective = true);
+	//Destructor
+	~Camera();
+
+	//Methods
+	//Getters
+	DirectX::XMFLOAT4X4 GetView();
+	DirectX::XMFLOAT4X4 GetProjection();
+	//Matrix Operations
+	//called once per frame
+	void UpdateViewMatrix();
+	//Called on initialization and when window resizes
+	void UpdateProjectionMatrix(float aspectRatio);
+	//Game loop
+	//processes user input, 
+	//adjusts the transform, 
+	//and updates view matrix
+	void Update(float dt);
 };
 

c9b36e45bc5a10d0f6097aa2d9c0d19be2049ea5
Nathan McAndrew (RIT Student) <nvm7252@rit.edu>
2025-04-02 16:43:41 -0400

    Created new pixel and vertex shaders. modified vertex shader to support normal maps

---
+++ NormalMapPS.hlsl
@@ -0,0 +1,62 @@
+#include "Structs.hlsli"
+#include "Lighting.hlsli"
+
+//data to manipulate pixels
+cbuffer ShaderData : register(b0)
+{
+	//material data
+	float3 colorTint;
+	float roughness;
+	float2 uvScale; //changes how many times texture is on object
+	float2 uvOffset; //changes start position of texture
+
+	//entity data
+	float3 cameraPos; //helps with specular + diffuse lighting
+
+	//light data
+	int numLights;
+	float3 ambientColor;
+
+	//light objects
+	Lights lights[MAX_LIGHTS];
+};
+
+Texture2D SurfaceTexture : register(t0); // "t" registers for textures
+SamplerState BasicSampler : register(s0); // "s" registers for samplers
+
+// --------------------------------------------------------
+// The entry point (main method) for our pixel shader
+// 
+// - Input is the data coming down the pipeline (defined by the struct)
+// - Output is a single color (float4)
+// - Has a special semantic (SV_TARGET), which means 
+//    "put the output of this into the current render target"
+// - Named "main" because that's the default the shader compiler looks for
+// --------------------------------------------------------
+float4 main(VertexToNormalMapPS input) : SV_TARGET
+{
+	//alter uv coords using offset
+	input.uv = input.uv * uvScale + uvOffset;
+
+	//normalize input normals
+	input.normal = normalize(input.normal);
+	
+	//"sample" the texture and color
+	//this gives output color
+	float3 color = SurfaceTexture.Sample(BasicSampler, input.uv).rgb; //swizzle using logical indices
+	//tint color with colorTint
+	color *= colorTint;
+	
+	//Begin lighting calculations
+	//include ambient lighting ONCE
+	float3 totalLight = color * ambientColor;
+	
+	//angle the surface is viewed from
+	float3 surfaceToCamera = normalize(cameraPos - input.worldPos);
+	
+	//apply the total lighting
+	totalLight += CalculateTotalLight(numLights, lights, input.normal, surfaceToCamera, input.worldPos, roughness, color);
+	
+	//return modified color
+	return float4(totalLight, 1);
+}
---
+++ NormalMapVS.hlsl
@@ -0,0 +1,61 @@
+#include "Structs.hlsli"
+
+//Constant buffer for modifying the appearance of meshes
+//Should match the "ShaderData" struct in BufferStructs.h
+//Names are shared between structs, but dont need to be
+//Currently modifies the color and position of meshes
+cbuffer ShaderData : register(b0)
+{
+	matrix world;
+	matrix worldInverseTranspose;
+	matrix view;
+	matrix projection;
+};
+
+// --------------------------------------------------------
+// The entry point (main method) for our vertex shader
+// 
+// - Input is exactly one vertex worth of data (defined by a struct)
+// - Output is a single struct of data to pass down the pipeline
+// - Named "main" because that's the default the shader compiler looks for
+// --------------------------------------------------------
+VertexToNormalMapPS main(VertexShaderInput input)
+{
+	// Set up output struct
+	VertexToPixel output;
+
+	// Here we're essentially passing the input position directly through to the next
+	// stage (rasterizer), though it needs to be a 4-component vector now.  
+	// - To be considered within the bounds of the screen, the X and Y components 
+	//   must be between -1 and 1.  
+	// - The Z component must be between 0 and 1.  
+	// - Each of these components is then automatically divided by the W component, 
+	//   which we're leaving at 1.0 for now (this is more useful when dealing with 
+	//   a perspective projection matrix, which we'll get to in the future).
+	// This is done using a matrix
+	// Order MATTERS as hlsl is Column-Major and D3D11 is Row-Major
+	//Multiply matrices for our camers
+	matrix worldViewProj = mul(projection, mul(view, world));
+	output.screenPosition = mul(worldViewProj, float4(input.localPosition, 1.0f));
+
+	//simply pass texture coord
+	output.uv = input.uv;
+
+	//3x3 ignores transformation operations
+	//world inverse transpose handles non uniform scaling
+	output.normal = mul((float3x3)worldInverseTranspose, input.normal);
+
+	//Rotate the tangent in a similar fashion
+	//No Inverse transpose is used because non-uniform scales won't result in incorrect tangents after normalization
+	output.tangent = mul((float3x3)world, input.tangent);
+
+	//Get world position of vertex, pixels will be handled by other pipeline stage
+	//convert local pos to a float 4
+	//then multiply by world matrix
+	//grab the three components (x, y, z) that we care about
+	output.worldPos = mul(world, float4(input.localPosition, 1)).xyz;
+
+	// Whatever we return will make its way through the pipeline to the
+	// next programmable stage we're using (the pixel shader for now)
+	return output;
+}

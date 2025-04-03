7baa7e07a2aec221dc202ead66404df85931917f
Nathan <nvm7252@rit.edu>
2025-04-03 19:31:13 -0400

    Completed constructors

--- Sky.h
+++ Sky.h
@@ -20,6 +20,42 @@ class Sky
 	std::shared_ptr<Mesh> cubeMesh;
 
 	//Constructor
-	Sky(std::shared_ptr<Mesh> mesh, Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler);
+	//with already loaded in resources
+	Sky(std::shared_ptr<Mesh> mesh,
+		Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler,
+		std::shared_ptr<SimpleVertexShader> vs,
+		std::shared_ptr<SimplePixelShader> ps,
+		Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> skySRV);
+
+	//loads in specific resources
+	Sky(std::shared_ptr<Mesh> mesh,
+		Microsoft::WRL::ComPtr<ID3D11SamplerState> sampler,
+		const wchar_t* cubeMapPath);
+
+	// --------------------------------------------------------
+	// Author: Chris Cascioli
+	// Purpose: Creates a cube map on the GPU from 6 individual textures
+	// 
+	// - You are allowed to directly copy/paste this into your code base
+	//   for assignments, given that you clearly cite that this is not
+	//   code of your own design.
+	//
+	// - Note: This code assumes you’re putting the function in Sky.cpp, 
+	//   you’ve included WICTextureLoader.h and you have an ID3D11Device 
+	//   ComPtr called “device”.  Make any adjustments necessary for
+	//   your own implementation.
+	// --------------------------------------------------------
+	
+	
+	// --- HEADER ---
+	
+	// Helper for creating a cubemap from 6 individual textures
+	Microsoft::WRL::ComPtr<ID3D11ShaderResourceView> CreateCubemap(
+		const wchar_t* right,
+		const wchar_t* left,
+		const wchar_t* up,
+		const wchar_t* down,
+		const wchar_t* front,
+		const wchar_t* back);
 };
 

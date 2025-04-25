1d29fb723bda8256e5f45aa052e43b5e8e5b4eb4
Nathan <nvm7252@rit.edu>
2025-04-25 14:39:07 -0400

    used bitwise operators to optimize BlurVS

--- BlurPPVS.hlsl
+++ BlurPPVS.hlsl
@@ -3,20 +3,14 @@
 VertexToPixelPP main(uint id : SV_VertexID)
 {
     VertexToPixelPP output;
-    if (id == 0)
-    {
-        output.position = float4(-1, 1, 0, 1);
-        output.uv = float2(0, 0);
-    }
-    else if (id == 1)
-    {
-        output.position = float4(3, 1, 0, 1);
-        output.uv = float2(2, 0);
-    }
-    else if (id == 2)
-    {
-        output.position = float4(-1, -3, 0, 1);
-        output.uv = float2(0, 2);
-    }
+    //calculate the UV (0,0) to (2,2) using the ID
+    output.uv = float2(
+    (id << 1) & 2, // Essentially: id % 2 * 2
+    id & 2);
+    //calculate the position based on the UV
+    output.position = float4(output.uv, 0, 1);
+    output.position.x = output.position.x * 2 - 1;
+    output.position.y = output.position.y * -2 + 1;
+
     return output;
 }
